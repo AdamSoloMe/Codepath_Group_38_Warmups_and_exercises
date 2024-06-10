1f953955f69f8684cecae71ec701e80b024f3a45
adam_solomon_home <adamsolomon1801@gmail.com>
2024-06-09 22:33:57 -0400

    updated comments for unit 2 warmup

--- Units/Unit_2/Warmups/Merged_Two_sorted_lists.py
+++ Units/Unit_2/Warmups/Merged_Two_sorted_lists.py
@@ -30,7 +30,19 @@ class Solution:
 
         return merged_list.next
 
-
+# 1) Instantiate a new node with a value of 0.
+#    This will be our dummy node, the node to the left of the head of our return list.
+#    Store a reference to this node so that we can return the list at the end.
+# 2) Create a pointer, tail, that always points to the end of our LL. Initialize it to point to the dummy node.
+# 3) Iterate over the LLs (while head1 or head2) where head1 and head2 are pointers to the heads of the input LLs
+#     a) If head1.val <= head2.val, append head1 to the list by pointing tail's next pointer to head1.
+#        Move head1 forward one node.
+#     b) Otherwise, append head2 to the list by pointing tail's next pointer to head2.
+#        Move head2 forward one node
+# 4) Move tail forward one node
+# 5) Figure out if either list still hasn't been fully traversed.
+#    If it hasn't, point tail's next to the head of the list that hasn't been fully traversed.
+# 6) Return dummy.next
 
 
 
